0298adc71a8e1a46a36146064b38626e25f67e05
James Thorne <james@jthorne.co.uk>
2021-06-11 16:09:23 +0100

    add license headers

--- src/error_correction/scoring/sari.py
+++ src/error_correction/scoring/sari.py
@@ -1,20 +1,20 @@
-# =======================================================
-#  SARI -- Text Simplification Tunable Evaluation Metric
-# =======================================================
 #
-# Author: Wei Xu (UPenn xwe@cis.upenn.edu)
+# Copyright (c) 2019-2021 James Thorne.
 #
-# A Python implementation of the SARI metric for text simplification
-# evaluation in the following paper
+# This file is part of factual error correction.
+# See https://jamesthorne.co.uk for further info.
 #
-#     "Optimizing Statistical Machine Translation for Text Simplification"
-#     Wei Xu, Courtney Napoles, Ellie Pavlick, Quanze Chen and Chris Callison-Burch
-#     In Transactions of the Association for Computational Linguistics (TACL) 2015
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
 #
-# There is also a Java implementation of the SARI metric
-# that is integrated into the Joshua MT Decoder. It can
-# be used for tuning Joshua models for a real end-to-end
-# text simplification model.
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 #
 
 from __future__ import division
